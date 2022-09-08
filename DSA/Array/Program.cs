8d3b6eb436b69e8b46bf493322a1d5396384e56a
Tahsin <tahsinhasanturab@gmail.com>
2022-09-08 20:07:02 +0600

    Array & BinarySearch

---
+++ DSA/Array/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] number = new int[10];
+var a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+number = a;
+
+//for(int i = 0; i < number.Length; i++)
+//{
+//    Console.WriteLine(number[i]);
+//}
+
+int n = int.Parse(Console.ReadLine());
+//Console.WriteLine(n);
+
+
+//Find index of n in array a
+int index = Array.IndexOf(a, n);
+if(index>=0 && index<number.Length)
+    Console.WriteLine(index);
+else Console.WriteLine("Not found");
---
+++ DSA/Binary_Search/Program.cs
@@ -0,0 +1,24 @@
+﻿var a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+
+Console.Write("Enter the element: ");
+int n = int.Parse(Console.ReadLine());
+
+int l = 0, r = a.Length - 1,pos=-1;
+while (l <= r)
+{
+    int mid = l + (r - l) / 2;
+    if (a[mid] == n)
+    {
+        pos = mid;
+        break;
+    }
+    else if (a[mid] < n)
+    {
+        l = mid + 1;
+    }
+    else r = mid - 1;
+}
+
+if (pos == -1) Console.WriteLine("Not found");
+else Console.WriteLine($"found at position {pos}");
