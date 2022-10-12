ba0d04e47676a157d38da0ad7f73a069c8118c4f
Tahsin <tahsinhasanturab@gmail.com>
2022-10-12 21:59:02 +0600

    DynamicArrayImplementation

---
+++ Core DataStructure/DS/ImplementationOfDynamicArray/Program.cs
@@ -0,0 +1,14 @@
+﻿using ImplementationOfDynamicArray;
+
+var v = new vector<int>();
+Console.WriteLine($"vector size {v.size}");
+v.push_back(1);
+v.push_back(2);
+v.push_back(3);
+Console.WriteLine($"vector size {v.size}");
+v.print();
+v.pop_back();
+Console.WriteLine($"vector size {v.size}");
+v.print();
+
+
---
+++ Core DataStructure/DS/ImplementationOfDynamicArray/vector.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplementationOfDynamicArray
+{
+    public class vector<T>
+    {
+        T[] arr = new T[2];
+
+        public int size { get; set; }
+        private int currentArrSize = 2;
+        public void push_back(T val)
+        {
+            size++;
+            if(size > currentArrSize)
+            {
+                T[] tempArr = new T[currentArrSize];
+                tempArr = arr;
+                
+                currentArrSize *= 2;
+               
+                arr = new T[currentArrSize];
+
+                for(int i = 0; i < size - 1; i++)
+                {
+                    arr[i]=tempArr[i];
+                }
+
+                arr[size - 1] = val;
+                
+            }
+            else
+            {
+                arr[size - 1] = val;
+            }
+
+
+        }
+        public void pop_back()
+        {
+            if (size > 0)
+            {
+                size--;
+            }
+        }
+
+        public void print()
+        {
+            for(int i = 0; i < size; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
