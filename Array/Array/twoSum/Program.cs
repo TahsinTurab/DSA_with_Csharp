028d0730e4b42354d99da5f377155c30066ce19b
Tahsin <tahsinhasanturab@gmail.com>
2023-10-15 10:12:14 +0600

    twoSum added

---
+++ Array/Array/twoSum/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var seen = new Dictionary<int, int>();
+        var ans = new int[2];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (seen.ContainsKey(target - nums[i]))
+            {
+                ans[0] = i;
+                ans[1] = seen[target - nums[i]];
+                return ans;
+            }
+            seen[nums[i]] = i;
+        }
+        return ans;
+    }
+}
